6724d89652dc8bc350a4dfa866e441e90eb45f3e
Fsesnick <fernandasesnick@gmail.com>
2021-02-16 11:28:24 +0000

    new prop book class

--- Book.js
+++ Book.js
@@ -1,7 +1,10 @@
 class Book {
-    constructor(title, author, isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
+constructor(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+  this.patron = null;
+  this.dueDate = null;
+  this.out = false;
+  }
 }
